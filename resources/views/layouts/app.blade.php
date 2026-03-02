a2d48cf94f5768ce5d3eed1e1f35cb883d3daf70
ETorres76 <administracion@crisab.com>
2026-03-01 22:12:57 -0500

    corregir tema oscuro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,20 +24,21 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-200">
+            
             @include('layouts.navigation')
-
-            <!-- Page Heading -->
+            
             @isset($header)
-                <header class="bg-white shadow">
+                <header class="bg-white dark:bg-gray-800 shadow transition-colors duration-200">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                        <div class="text-gray-800 dark:text-gray-200">
+                            {{ $header }}
+                        </div>
                     </div>
                 </header>
             @endisset
 
-            <!-- Page Content -->
-            <main>
+            <main class="text-gray-900 dark:text-gray-100">
                 {{ $slot }}
             </main>
         </div>
